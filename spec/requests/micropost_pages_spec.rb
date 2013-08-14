f82d96c835eef9c609635e095e38953ef07be197
Ed Trembicki-Guy <edtrembickiguy@iname.com>
2013-08-14 13:53:49 -0400

    Chap 10 exercises

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -24,6 +24,9 @@ describe "MicropostPages" do
     
     describe "with valid information" do
     
+      describe "zero micropost state" do
+        it { should have_content('0 microposts') }
+      end
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
@@ -31,11 +34,28 @@ describe "MicropostPages" do
     end
   end
   
+  describe "one micropost state" do
+    it "should have one micropost" do
+      expect { Micropost.count == 1 }
+    end
+  end
+
   describe "micropost destruction" do
     before { FactoryGirl.create(:micropost, user: user) }
     
     describe "as correct user" do
       before {visit root_path}
+      
+      describe "one micropost state" do
+        it { should have_content('1 micropost') }
+        it { should_not have_content('1 microposts') }
+      end
+      
+      describe "two micropost state" do
+        before { FactoryGirl.create(:micropost, user: user) }
+        before { visit root_path }
+        it { should have_content('2 microposts') }
+      end
       
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
